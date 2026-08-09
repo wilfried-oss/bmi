664c7271d20f1a8dbcb779fd08c04972fd04632e
wilfried-oss <davowilfried3@gmail.com>
2026-08-09 19:16:29 +0100

    modified api.py + test_api.py for bmi calculator implementation

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -10,18 +9,33 @@ class BMICalculatorRequest(BaseModel):
     weight: float
 
 
+@app.get("/")
+def welcome_route():
+    return {"message": "Welcome de BMI Calculator API"}
+
+
 @app.get("/healthz")
 def health_check():
     return {"status": "ok"}
 
 
-def _calculate_bmi(height: float, weight: float) -> float:
+def _calculate_bmi(weight: float, height: float) -> float:
     if height <= 0 or weight <= 0:
-        raise HTTPException(status_code=400, detail="Height and weight must be positive values.")
-    return weight / (height ** 2)
+        raise HTTPException(
+            status_code=400, detail="Height and weight must be positive values."
+        )
+    return weight / (height**2)
 
 
-
-def _input_validation(height: float, weight: float):
+def _input_validation(weight: float, height: float):
     if height <= 0 or weight <= 0:
-        raise HTTPException(status_code=400, detail="Height and weight must be positive values.")
+        raise HTTPException(
+            status_code=400, detail="Height and weight must be positive values."
+        )
+
+
+@app.post("/calculate_bmi")
+def calculate_bmi(request: BMICalculatorRequest):
+    _input_validation(request.weight, request.height)
+    bmi = _calculate_bmi(request.weight, request.height)
+    return {"bmi": bmi}
